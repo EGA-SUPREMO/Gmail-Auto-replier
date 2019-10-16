e03f037c0a2e3d91063c3e269dd6add958f2fbfc
Ale <cxiesestro@gmail.com>
2019-10-16 17:09:58 -0400

    Add files via upload

--- hope.py
+++ hope.py
@@ -2,9 +2,9 @@ import easyimap
 
 host = 'imap.gmail.com'
 user = 'cxiesestro@gmail.com'
-password = 'secrectPlease'
+password = input('password: ')
 mailbox = 'INBOX.subfolder'
-imapper = easyimap.connect(host, user, password, mailbox)
+imapper = easyimap.connect(host, user, password, mailbox, ssl=False, port=993)
 
 email_quantity = 3
 emails_from_your_mailbox = imapper.listids(limit=email_quantity)
