78f2f00c263b876883eb40d5374ab1ea0f86706d
Alejandro Garcia <cxiesestro@gmail.com>
2020-07-05 10:20:50 -0400

    Delete date.py (#8)

--- date.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-
-def getNow():
-    return datetime.utcnow()
-#timedelta(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0)
-
-
-#datetime(2009, 1, 6, 15, 8, 24, 78915)
